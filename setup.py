64fb38f4c07403f0be86743c968f57d3564d5d97
Iago López <ilopezsmx2011@gmail.com>
2023-09-26 11:49:16 +0200

    Back to development: 1.2

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='genweb6.gpaq',
-    version='1.1',
+    version='1.2.dev0',
     description="Genweb 6 GPAQ package",
     long_description=long_description,
     # Get more from https://pypi.org/classifiers/
